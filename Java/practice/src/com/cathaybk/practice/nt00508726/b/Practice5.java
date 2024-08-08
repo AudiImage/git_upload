6cf29905631501f9600b28bf8f6f75c571ef843e
audi_00508726 <audi51508@gmail.com>
2024-08-08 16:07:04 +0800

    fix practice 5

--- Java/practice/src/com/cathaybk/practice/nt00508726/b/Practice5.java
+++ Java/practice/src/com/cathaybk/practice/nt00508726/b/Practice5.java
@@ -17,9 +17,17 @@ public class Practice5 {
         int days = 0;
 
         //接受用户输入的年份
-        Scanner in = new Scanner(System.in);
-        System.out.print("輸入介於1-12之間的整數m:");
-        int month = in.nextInt();
+        int month;
+        while(true){
+            Scanner in = new Scanner(System.in);
+            System.out.print("輸入介於1-12之間的整數m:");
+            month = in.nextInt();
+            if(month >= 1 && month <= 12){
+                break;
+            }
+        }
+
+
 
         //得到一个Calendar对象
         Calendar c = Calendar.getInstance();
@@ -28,9 +36,8 @@ public class Practice5 {
         System.out.printf("      %d年%d月            \n", year, month);
         System.out.println("--------------------------");
         System.out.println("日\t 一\t 二\t 三\t 四\t 五\t 六");
-        System.out.println("==========================");
+        System.out.println("===========================");
 
-        // 判断当前月份的天数
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             days = 31;
         }
@@ -38,7 +45,6 @@ public class Practice5 {
             days = 30;
         }
         if (month == 2) {
-            //闰年判断
             if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
                 days = 29;
             } else {
@@ -48,19 +54,16 @@ public class Practice5 {
 
 
         c.set(year, month - 1, 1);
-        //求出第一天是星期几
+
         int FirstDayInWeek = c.get(Calendar.DAY_OF_WEEK) - 1;
 
-        //用来控制换行
         int cnt = 0;
 
-        //前面的空位
         for (int j = 0; j < FirstDayInWeek; j++) {
             System.out.print("    ");
             cnt++;
         }
 
-        //打印当前月份日历
         for (int i = 1; i <= days; i++) {
             if (cnt == 7) {
                 System.out.printf("\n");
